2f2f6750b23cad2579dd22b89ac6fa9047a6ceb4
21Neil <88711171+21Neil@users.noreply.github.com>
2024-10-12 10:21:30 +0800

    Add calculator function

--- testing_practice.js
+++ testing_practice.js
@@ -8,3 +8,16 @@ export function reverseString(str) {
   const arr = str.split('')
   return arr.reverse().join('')
 }
+
+export const calculator = (function () {
+  const add = (num1, num2) => num1 + num2
+  const subtract = (num1, num2) => num1 - num2
+  const divide = (num1, num2) => num1 / num2
+  const multiply = (num1, num2) => num1 * num2
+  return {
+    add,
+    subtract,
+    divide,
+    multiply,
+  } 
+})()
--- testing_practice.test.js
+++ testing_practice.test.js
@@ -1,6 +1,7 @@
 import {
   capitalize,
   reverseString,
+  calculator,
 } from'./testing_practice'
 
 test('neil->Neil', () => {
@@ -9,4 +10,20 @@ test('neil->Neil', () => {
 
 test('test->tset', () => {
   expect(reverseString('test')).toBe('tset')
+})
+
+describe('calculator', () => {
+  console.log(calculator.add(1, 1))
+  test('5 + 6', () => {
+    expect(calculator.add(5, 6)).toBe(11)
+  })
+  test('11 - 5', () => {
+    expect(calculator.subtract(11, 5)).toBe(6)
+  })
+  test('12 / 3', () => {
+    expect(calculator.divide(12, 3)).toBe(4)
+  })
+  test('4 * 5', () => {
+    expect(calculator.multiply(4, 5)).toBe(20)
+  })
 })
